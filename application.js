d40eb0668de043663167bd7d906bd7c628bba902
luketheterrible <lukehenry4@gmail.com>
2014-01-15 22:17:20 -0600

    Style changes and Javascript

--- application.js
+++ application.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 
+	$('#item').keyup(function(event){
+		if(event.keyCode == 13) {
+			$('button').click();
+		}
+	});	
+
+	$('button').click(function(){
+		var txtbox = document.getElementById('item');
+		var txtval = txtbox.value;
+		$('#list').append('<li>' + txtval + '</li>');
+		document.getElementById('item').value = '';
+	});
+
 });
