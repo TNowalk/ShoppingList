443aa7957d0151c3e2a36ee629a8fc003fd50dde
luketheterrible <lukehenry4@gmail.com>
2014-01-16 09:42:41 -0600

    Update

--- application.js
+++ application.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
 
+	$('#item').keyup(function(event){
+		if(event.keyCode == 13) {
+			$('button').click();
+		}
+	});	
+
+	$('button').click(function(){
+		var txtbox = document.getElementById('item');
+		var txtval = txtbox.value;
+		$('#list').append('<li>' + txtval + '</li>');
+		document.getElementById('item').value = '';
+	});
+
+
+
 });
+
+//comment
